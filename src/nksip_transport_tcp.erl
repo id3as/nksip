0644678ab728aeed5a8bd06a93070b3e211445db
Philip Stears <philip@philipstears.com>
2019-01-04 15:42:15 +0100

    Altered nksip's hacks so they work with new ranch. :rage:

--- src/nksip_transport_tcp.erl
+++ src/nksip_transport_tcp.erl
@@ -25,7 +25,7 @@
 -author('Carlos Gonzalez <carlosj.gf@gmail.com>').
 
 -export([get_listener/3, connect/2]).
--export([ranch_start_link/6, start_link/4]).
+-export([ranch_start_link/5, start_link/4]).
 
 -include("nksip.hrl").
 -include("nksip_call.hrl").
@@ -49,21 +49,21 @@ get_listener(AppId, Transp, Opts) ->
     end,
     Timeout = 1000 * nksip_sipapp_srv:config(AppId, tcp_timeout),
     Spec = ranch:child_spec(
-        {AppId, Proto, Ip, Port}, 
-        Listeners, 
+        {AppId, Proto, Ip, Port},
+        Listeners,
         Module,
-        listen_opts(Proto, Ip, Port, Opts), 
+        listen_opts(Proto, Ip, Port, Opts),
         ?MODULE,
         [AppId, Transp, Timeout]),
     % Little hack to use our start_link instead of ranch's one
     {ranch_listener_sup, start_link, StartOpts} = element(2, Spec),
     setelement(2, Spec, {?MODULE, ranch_start_link, StartOpts}).
 
-    
+
 %% @private Starts a new connection to a remote server
 -spec connect(nksip:app_id(), nksip:transport()) ->
     {ok, term()} | {error, term()}.
-         
+
 connect(AppId, Transp) ->
     case nksip_connection:is_max(AppId) of
         false ->
@@ -74,7 +74,7 @@ connect(AppId, Transp) ->
                 tls -> {ssl, ssl}
             end,
             case TranspMod:connect(Ip, Port, SocketOpts) of
-                {ok, Socket} -> 
+                {ok, Socket} ->
                     {ok, {LocalIp, LocalPort}} = InetMod:sockname(Socket),
                     Transp1 = Transp#transport{
                         local_ip = LocalIp,
@@ -85,7 +85,7 @@ connect(AppId, Transp) ->
                     Timeout = 1000 * nksip_sipapp_srv:config(AppId, tcp_timeout),
                     Spec = {
                         {AppId, Proto, Ip, Port, make_ref()},
-                        {nksip_connection, start_link, 
+                        {nksip_connection, start_link,
                             [AppId, Transp1, Socket, Timeout]},
                         temporary,
                         5000,
@@ -138,17 +138,21 @@ outbound_opts(tls, AppId) ->
 
 
 %% @private Gets socket options for listening connections
--spec listen_opts(nksip:protocol(), inet:ip_address(), inet:port_number(), 
+-spec listen_opts(nksip:protocol(), inet:ip_address(), inet:port_number(),
                     nksip:optslist()) ->
     nksip:optslist().
 
 listen_opts(tcp, Ip, Port, Opts) ->
-    Max = nksip_lib:get_value(max_connections, Opts, 100),
-    [
-        {ip, Ip}, {port, Port}, {active, false}, 
-        {nodelay, true}, {keepalive, true}, {packet, raw},
-        {max_connections, Max}
-    ];
+  Max = nksip_lib:get_value(max_connections, Opts, 100),
+  #{ socket_opts => [ {ip, Ip}
+                    , {port, Port}
+                    , {active, false}
+                    , {nodelay, true}
+                    , {keepalive, true}
+                    , {packet, raw}
+                    ]
+   , max_connections => Max
+   };
 
 listen_opts(tls, Ip, Port, Opts) ->
     case code:priv_dir(nksip) of
@@ -163,7 +167,7 @@ listen_opts(tls, Ip, Port, Opts) ->
     Key = nksip_lib:get_value(keyfile, Opts, DefKey),
     Max = nksip_lib:get_value(max_connections, Opts, 100),
     lists:flatten([
-        {ip, Ip}, {port, Port}, {active, false}, 
+        {ip, Ip}, {port, Port}, {active, false},
         {nodelay, true}, {keepalive, true}, {packet, raw},
         {max_connections, Max},
         case Cert of "" -> []; _ -> {certfile, Cert} end,
@@ -172,14 +176,9 @@ listen_opts(tls, Ip, Port, Opts) ->
 
 
 %% @private Our version of ranch_listener_sup:start_link/5
--spec ranch_start_link(any(), non_neg_integer(), module(), term(), module(), term())-> 
-    {ok, pid()}.
-
-ranch_start_link(Ref, NbAcceptors, RanchTransp, TransOpts, Protocol, 
-                    [AppId, Transp, Timeout]) ->
-    case 
-        ranch_listener_sup:start_link(Ref, NbAcceptors, RanchTransp, TransOpts, 
-                                      Protocol, [AppId, Transp, Timeout])
+ranch_start_link(Ref, RanchTransp, TransOpts, Protocol, [AppId, Transp, Timeout]) ->
+    case
+        ranch_listener_sup:start_link(Ref, RanchTransp, TransOpts, Protocol, [AppId, Transp, Timeout])
     of
         {ok, Pid} ->
             Port = ranch:get_port(Ref),
@@ -190,7 +189,7 @@ ranch_start_link(Ref, NbAcceptors, RanchTransp, TransOpts, Protocol,
         Other ->
             Other
     end.
-   
+
 
 %% @private Ranch's callback, called for every new inbound connection
 -spec start_link(pid(), port(), atom(), term()) ->
@@ -209,5 +208,3 @@ start_link(_ListenerPid, Socket, Module, [AppId, Transp, Timeout]) ->
     },
     Module:setopts(Socket, [{nodelay, true}, {keepalive, true}]),
     nksip_connection:start_link(AppId, Transp1, Socket, Timeout).
-
-
